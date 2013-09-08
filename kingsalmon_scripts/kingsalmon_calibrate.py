d4dbe53315515c9893cde3f6d2049e3511013389
Jon Klein <jtklein@alaska.edu>
2013-09-08 00:49:06 -0800

    fixed bug in qnx beam control, commented kingsalmon_calibrate and added TODOs

--- kingsalmon_scripts/kingsalmon_calibrate.py
+++ kingsalmon_scripts/kingsalmon_calibrate.py
@@ -1,37 +1,47 @@
 # script to measure save a bunch of VNA phase measurements while stepping beam numbers
 # useful for characterizing the RF path (transmitter antenna port to receiver input) and looking for time delay differences
 # requires ssh key for QNX box and VNA
-# jon klein, jtklein@alaska.edu
+# jon klein, jtklein@alaska.edu, mit license
 
 from pylab import *
 from vna_control import *
 from qnx_beamcontrol import *
-import argparse, csv
+import argparse, csv, os
 
 SWEEP_CENTER = 13e6
 SWEEP_SPAN = 10e6
-SWEEP_POINTS = 801
+SWEEP_POINTS = 201
 
 BEAMS = 16
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
     parser.add_argument("--cal", action="count", help="run through calibration on VNA before taking measurements", default=0)
     parser.add_argument("--vnaip", help="specify VNA ip address", default=VNAHOST)
     parser.add_argument("--qnxip", help="specify QNX ip address", default=QNX_IP)
+    parser.add_argument("--ddir", help="specify a directory to save the data in", default='sandbox')
     parser.add_argument("--b", type=int, help="specify number of beams", default=BEAMS)
     parser.add_argument("--cards", type=int, help="specify number of cards to calibrate", default=1)
 
     args = parser.parse_args()
+
+    # open connection with VNA
     vna = lan_init(args.vnaip)
 
+    # calibrate VNA if run with --cal
+    # TODO: calibration is undertested... verify this
     if args.cal:
         print 'calibrating VNA'
         vna_preset(vna)
         vna_through_cal(vna)
 
-
+    # set VNA span
     vna_setspan(vna, SWEEP_SPAN, SWEEP_CENTER, SWEEP_POINTS):
+   
+    # step through each card and measure phase at each beam setting
+    # TODO: save this data in a useful format
+    # TODO: log magnitude data and complain if it is unreasonable
 
     for c in range(args.cards):
         for b in range(args.b):
--- kingsalmon_scripts/qnx_beamcontrol.py
+++ kingsalmon_scripts/qnx_beamcontrol.py
@@ -7,6 +7,6 @@ QNX_IP = '192.168.04'
 BEAM_SETTLE = .1
 
 def qnx_setbeam(ip, beam):
-    os.system('ssh root@' + QNX_IP + '"dio_set_beam -b "' + str(int(beam)))
+    os.system('ssh root@' + QNX_IP + '"dio_set_beam -b ' + str(int(beam)) + '"')
     time.sleep(BEAM_SETTLE)
 
